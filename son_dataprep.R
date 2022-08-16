120236425dc643630dd64d37c10342c7ae55f0a7
niwunder <104715287+niwunder@users.noreply.github.com>
2022-08-16 12:53:30 +0200

    Rename son_preprocessing.R to son_dataprep.R

--- son_dataprep.R
+++ son_dataprep.R
@@ -65,61 +65,64 @@ son_gr <- son %>%
   group_by(sound) %>%
   summarise(
     sum_favourite = sum(favourite, na.rm = T),
-    mean_pre_loudness = mean(pre_loudness), #maybe other than mean?
+    mean_pre_loudness = mean(pre_loudness),
+    sd_pre_loudness = sd(pre_loudness),
     mean_pre_distress = mean(pre_distress),
+    sd_pre_distress = sd(pre_distress),
     mean_pause_count = mean(pause_count, na.rm = T),
+    sd_pause_count = sd(pause_count, na.rm = T),
     mean_listen_time = mean(listen_time), #not correct, because there are negative values
+    sd_listen_time = sd(listen_time),
     mean_longest_listen_time = mean(longest_listen_time),
+    sd_longest_listen_time = sd(longest_listen_time),
     mean_post_loudness = mean(post_loudness, na.rm = T),
+    sd_post_loudness = sd(post_loudness, na.rm = T),
     mean_delta_sound_rating = mean(delta_sound_rating, na.rm = T),
     sound_origin = NA,
     sound_mechanism = NA
   )
 
 
+#add sound_energy: grouping sounds to tin+ and tin-
 
-## ----------------------------------------------------------------------------
-#add sound_mechanism: grouping sounds to masking and lateral inhibition
-#sound masking = 1, lateral inhibition = 2
-
-#create a vector with half the masking sounds
-sounds_masking <- c("sparrows", "seagulls", "churchbells", "kettle", "gong", "notchedmeditation", "whales", "mice", 
+#create a vector with half the tin_plus sounds
+sounds_tin_plus <- c("sparrows", "seagulls", "churchbells", "kettle", "gong", "notchedmeditation", "whales", "mice", 
                     "screamingkids", "laughingkids", "notchedmusic", "creakingdoor", "teethbrushing", "frogs", "writing", "cicada")
 #bird chirp = sparrows? // scribbling = writing?
 
-#add the rest of the sounds comfortably
-sounds_masking <- c(sounds_masking, grep("fm_", son_gr$sound, value = T))
-sounds_masking <- c(sounds_masking, grep("_bp", son_gr$sound, value = T))
-sounds_masking <- c(sounds_masking, grep("_notch", son_gr$sound, value = T))
-sounds_masking <- c(sounds_masking, grep("tin_", son_gr$sound, value = T))
+#add the rest of the tin_plus sounds comfortably
+sounds_tin_plus <- c(sounds_tin_plus, grep("fm_", son_gr$sound, value = T))
+sounds_tin_plus <- c(sounds_tin_plus, grep("_bp", son_gr$sound, value = T))
+sounds_tin_plus <- c(sounds_tin_plus, grep("_notch", son_gr$sound, value = T))
+sounds_tin_plus <- c(sounds_tin_plus, grep("tin_", son_gr$sound, value = T))
 
 
-#write sound_mechanism into son
-#if str from son_gr$sound is in sounds_masking, write son$sound_mechanism[i] = 1
+#write sound_energy into son
+#if str from son_gr$sound is in sounds_tin_plus, write son$sound_energy[i] = 1
 for (i in 1:length(son$sound)){
-  if (son$sound[i] %in% sounds_masking) {
-    son$sound_mechanism[i] <- 1
+  if (son$sound[i] %in% sounds_tin_plus) {
+    son$sound_energy[i] <- "tin+"
   } else {
-    son$sound_mechanism[i] <- 2
+    son$sound_energy[i] <- "tin-"
   }
 }
-son$sound_mechanism <- as.factor(son$sound_mechanism)
+son$sound_energy <- as.factor(son$sound_energy)
 
 
-#write sound_mechanism into son_gr
+#write sound_energy into son_gr
 for (i in 1:length(son_gr$sound)){
-  if (son_gr$sound[i] %in% sounds_masking) {
-    son_gr$sound_mechanism[i] <- 1
+  if (son_gr$sound[i] %in% sounds_tin_plus) {
+    son_gr$sound_energy[i] <- "tin+"
   } else {
-    son_gr$sound_mechanism[i] <- 2
+    son_gr$sound_energy[i] <- "tin-"
   }
 }
-son_gr$sound_mechanism <- as.factor(son_gr$sound_mechanism)
+son_gr$sound_energy <- as.factor(son_gr$sound_energy)
+
 
 
 ## ----------------------------------------------------------------------------
-#add sound_origin: grouping sounds to natural and artificial
-#natural sounds = 1, synthetic sounds = 2
+#add sound_origin: grouping sounds to natural and synthetic
 
 #create a vector with all the natural sounds
 sounds_natural <- c("sparrows", "seagulls", "churchbells", "kettle", "gong", "notchedmeditation", "whales", 
@@ -130,9 +133,9 @@ sounds_natural <- c("sparrows", "seagulls", "churchbells", "kettle", "gong", "no
 #write sound_origin into son
 for (i in 1:length(son$sound)){
   if (son$sound[i] %in% sounds_natural) {
-    son$sound_origin[i] <- 1
+    son$sound_origin[i] <- "nat"
   } else {
-    son$sound_origin[i] <- 2
+    son$sound_origin[i] <- "syn"
   }
 }
 son$sound_origin <- as.factor(son$sound_origin)
@@ -141,9 +144,9 @@ son$sound_origin <- as.factor(son$sound_origin)
 #write sound_origin into son_gr
 for (i in 1:length(son_gr$sound)){
   if (son_gr$sound[i] %in% sounds_natural) {
-    son_gr$sound_origin[i] <- 1
+    son_gr$sound_origin[i] <- "nat"
   } else {
-    son_gr$sound_origin[i] <- 2
+    son_gr$sound_origin[i] <- "syn"
   }
 }
 son_gr$sound_origin <- as.factor(son_gr$sound_origin)
@@ -164,7 +167,7 @@ write.csv2(son_gr, file = "son_groupedby_sound.csv")
 
 
 ## ----------------------------------------------------------------------------
-## PROBLEMS
+## PROBLEMS / OPEN TASKS
 
-#1: negative listen_time
-#2: were sounds matched?
+#1: negative listen_time in data set
+#2: create variable in data set son that contains tinnitus frequency (=matching) values
